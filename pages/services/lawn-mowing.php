ebd2e9ce863c8f07b563d7ce51b46d9ff94057b8
satyam mishra <satyamjay007@gmail.com>
2023-08-24 12:19:58 +0530

    services section added

--- pages/services/lawn-mowing.php
+++ pages/services/lawn-mowing.php
@@ -98,9 +98,9 @@
                 </svg>
                 <h2 class="sub-heading">Lawn Mowing Services, Made Easy
                 </h2>
-                <h2 class="sub-heading">Why we’re the best rated in lawn mowing</h2>
             </div>
             <div class="second-services-content">
+                <h2 class="sub-heading">Why we’re the best rated in lawn mowing</h2>
                 <h2 class="sub-heading">
                     1. We find lawn mowing pros near you.
                 </h2>
@@ -122,6 +122,173 @@
         </div>
     </section>
 
+    <section class="lawn-questions-section">
+        <div class="lawn-questions-start">
+            <h2 class="sub-heading">
+                2. We offer immediate and transparent pricing on all mowing jobs.
+            </h2>
+            <p class="text">
+                We also solve the age old problem of determining a fair price for lawn mowing services. We quote your lawn mowing service cost instantly using our proprietary satellite technology. You can feel confident knowing that our price is fair and consistent. You know what you'll pay before we even schedule the job. We aim to provide affordable lawn care services for everyone.
+            </p>
+            <h2 class="sub-heading">
+                3. Our lawn mowing pros do great work, we guarantee it.
+            </h2>
+            <p class="text">
+                Lawn care and maintenance takes time and the right equipment to do it right. We’ll connect you with a lawn company that is prepared to handle even the most complex lawn mowing needs. When hiring someone to care for your yard, you want to make sure it's a company you can trust. Your lawn care professional has already been pre-screened and vetted to ensure only the best lawn services are provided. You can rest assured that your yard mowing services will get done right or you don't pay us. It's that simple. Just ask our happy customers.
+            </p>
+        </div>
+        <div class="lawn-questions-img">
+            <img src="<?= get_img() ?>/bearded-man-mowing-lawn.jpg" alt="bearded man mowing lawn">
+        </div>
+        <div class="lawn-questions-second">
+            <div class="flex">
+                <svg viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg">
+                    <g fill="none" fill-rule="evenodd">
+                        <path d="M4.72 14.018L22.678 2.646a8 8 0 018.412-.092l19.042 11.487A8 8 0 0154 20.89v18.752a8 8 0 01-3.985 6.92L30.97 57.612a8 8 0 01-8.177-.088L4.838 46.588A8 8 0 011 39.756V20.777a8 8 0 013.72-6.759z" class="stroke-current" stroke-linejoin="round" stroke-width="2"></path>
+                        <path class="fill-current" d="M33.903 32a69.355 69.355 0 01-1.532 2.319c4.653.684 7.035 2.15 7.035 2.88 0 .971-4.22 3.248-12.406 3.248-8.185 0-12.406-2.277-12.406-3.248 0-.73 2.381-2.195 7.035-2.88A69.41 69.41 0 0120.097 32C15.506 32.852 12 34.603 12 37.199 12 41.007 19.547 43 27 43c7.454 0 15-1.993 15-5.801 0-2.596-3.504-4.347-8.097-5.199z"></path>
+                        <path class="fill-current" d="M26.994 13.304c-4.781 0-8.657 3.91-8.657 8.734 0 3.46 4.453 10.08 6.972 13.549.394.542 1.02.862 1.685.862a2.08 2.08 0 001.685-.862c2.518-3.468 6.972-10.089 6.972-13.55 0-4.823-3.876-8.733-8.657-8.733zm0 14.157c-2.825 0-5.123-2.319-5.123-5.168 0-2.85 2.298-5.17 5.123-5.17s5.123 2.32 5.123 5.17-2.298 5.168-5.123 5.168z"></path>
+                        <path class="fill-current" d="M26.994 19.149c-1.718 0-3.116 1.41-3.116 3.144 0 1.732 1.398 3.143 3.116 3.143 1.718 0 3.116-1.41 3.116-3.143 0-1.735-1.398-3.144-3.116-3.144z"></path>
+                    </g>
+                </svg>
+                <h2 class="sub-heading">Do I need lawn mowing services?
+                </h2>
+            </div>
+            <p class="text">Grass cutting is typically a part of most lawn owner’s weekly chore list. But what if there were an easier way? If you had some help, how much time would it save you, and would your lawn end up looking better and being healthier in the end?</p>
+
+            <h2 class="sub-heading">How long does it take to mow a lawn?</h2>
+            <p class="text"> It takes about an hour to mow the average sized lawn, which ends up being more than 20 hours a year! That’s time you could be learning to play the guitar, taking an online class, trying out some new recipes, or just relaxing outside.</p>
+
+            <h2 class="sub-heading">Are you mowing correctly?</h2>
+            <p class="text"> While lawn mowing may seem simple, every time you cut your grass, you are either one step closer to lush and healthy looking grass or crippling its chances of survival. Proper mowing doesn't only give your lawn an attractive appearance, it also increases the density of your grass. When done by a professional, you may actually notice a big difference!</p>
+        </div>
+
+        <div class="lawn-questions-second">
+            <div class="flex">
+                <svg viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg">
+                    <g fill="none" fill-rule="evenodd">
+                        <path d="M4.72 14.018L22.678 2.646a8 8 0 018.412-.092l19.042 11.487A8 8 0 0154 20.89v18.752a8 8 0 01-3.985 6.92L30.97 57.612a8 8 0 01-8.177-.088L4.838 46.588A8 8 0 011 39.756V20.777a8 8 0 013.72-6.759z" class="stroke-current" stroke-linejoin="round" stroke-width="2"></path>
+                        <path class="fill-current" d="M33.903 32a69.355 69.355 0 01-1.532 2.319c4.653.684 7.035 2.15 7.035 2.88 0 .971-4.22 3.248-12.406 3.248-8.185 0-12.406-2.277-12.406-3.248 0-.73 2.381-2.195 7.035-2.88A69.41 69.41 0 0120.097 32C15.506 32.852 12 34.603 12 37.199 12 41.007 19.547 43 27 43c7.454 0 15-1.993 15-5.801 0-2.596-3.504-4.347-8.097-5.199z"></path>
+                        <path class="fill-current" d="M26.994 13.304c-4.781 0-8.657 3.91-8.657 8.734 0 3.46 4.453 10.08 6.972 13.549.394.542 1.02.862 1.685.862a2.08 2.08 0 001.685-.862c2.518-3.468 6.972-10.089 6.972-13.55 0-4.823-3.876-8.733-8.657-8.733zm0 14.157c-2.825 0-5.123-2.319-5.123-5.168 0-2.85 2.298-5.17 5.123-5.17s5.123 2.32 5.123 5.17-2.298 5.168-5.123 5.168z"></path>
+                        <path class="fill-current" d="M26.994 19.149c-1.718 0-3.116 1.41-3.116 3.144 0 1.732 1.398 3.143 3.116 3.143 1.718 0 3.116-1.41 3.116-3.143 0-1.735-1.398-3.144-3.116-3.144z"></path>
+                    </g>
+                </svg>
+                <h2 class="sub-heading">Do I need lawn mowing services?
+                </h2>
+            </div>
+            <p class="text">
+                Our goal is to provide cheap lawn mowing services, without compromising quality. Payment and scheduling are automated, and you can manage everything from the convenience of your phone or web browser. If you’re looking for affordable lawn care, lawn care services start at just $29. We have different options for mowing and maintenance services and plans to fit any budget. Our pricing is transparent, meaning you'll know what you'll pay for your service before the job is even scheduled so you'll never experience any hidden fees.
+                <br>
+                Come see what the best lawn care of the future looks like. Let Lawn Love connect you with a nearby lawn care professional today.
+            </p>
+        </div>
+
+        <div class="lawn-questions-second">
+            <div class="flex">
+                <svg viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg">
+                    <g fill="none" fill-rule="evenodd">
+                        <path d="M4.72 14.018L22.678 2.646a8 8 0 018.412-.092l19.042 11.487A8 8 0 0154 20.89v18.752a8 8 0 01-3.985 6.92L30.97 57.612a8 8 0 01-8.177-.088L4.838 46.588A8 8 0 011 39.756V20.777a8 8 0 013.72-6.759z" class="stroke-current" stroke-linejoin="round" stroke-width="2"></path>
+                        <path class="fill-current" d="M33.903 32a69.355 69.355 0 01-1.532 2.319c4.653.684 7.035 2.15 7.035 2.88 0 .971-4.22 3.248-12.406 3.248-8.185 0-12.406-2.277-12.406-3.248 0-.73 2.381-2.195 7.035-2.88A69.41 69.41 0 0120.097 32C15.506 32.852 12 34.603 12 37.199 12 41.007 19.547 43 27 43c7.454 0 15-1.993 15-5.801 0-2.596-3.504-4.347-8.097-5.199z"></path>
+                        <path class="fill-current" d="M26.994 13.304c-4.781 0-8.657 3.91-8.657 8.734 0 3.46 4.453 10.08 6.972 13.549.394.542 1.02.862 1.685.862a2.08 2.08 0 001.685-.862c2.518-3.468 6.972-10.089 6.972-13.55 0-4.823-3.876-8.733-8.657-8.733zm0 14.157c-2.825 0-5.123-2.319-5.123-5.168 0-2.85 2.298-5.17 5.123-5.17s5.123 2.32 5.123 5.17-2.298 5.168-5.123 5.168z"></path>
+                        <path class="fill-current" d="M26.994 19.149c-1.718 0-3.116 1.41-3.116 3.144 0 1.732 1.398 3.143 3.116 3.143 1.718 0 3.116-1.41 3.116-3.143 0-1.735-1.398-3.144-3.116-3.144z"></path>
+                    </g>
+                </svg>
+                <h2 class="sub-heading">Why Lawn love ?
+                </h2>
+            </div>
+            <p class="text">
+                Using our lawn care app, lawn care providers have access to pick up your job in as little as two days. Get an instant quote online based on your exact location and size of your lawn.
+                <br>
+                Lawn Love is a modern, delightful lawn care experience for the best mowing service.
+                <br>
+                Gone are the days of in-person estimates, payments left under the doormat, and spotty services from lawn companies. Lawn care doesn’t have to be a hassle.
+                <br>
+                Our quoting technology will generate a personalized quote for your yard - all in under two minutes. From there, we'll send over only skilled, pre-screened lawn care pros from one of the best lawn care companies to work their magic. Your pro will come prepared with all their own lawn care tools. You don’t even need to be home!
+            </p>
+        </div>
+    </section>
+
+    <!-- ------------------------ -->
+    <!-- Review tenth card section -->
+    <!-- ------------------------ -->
+    <?php include reviews; ?>
+
+
+    <section class="lawn-questions-section">
+        <div class="lawn-questions-second">
+            <div class="flex">
+                <svg viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg">
+                    <g fill="none" fill-rule="evenodd">
+                        <path d="M4.72 14.018L22.678 2.646a8 8 0 018.412-.092l19.042 11.487A8 8 0 0154 20.89v18.752a8 8 0 01-3.985 6.92L30.97 57.612a8 8 0 01-8.177-.088L4.838 46.588A8 8 0 011 39.756V20.777a8 8 0 013.72-6.759z" class="stroke-current" stroke-linejoin="round" stroke-width="2"></path>
+                        <path class="fill-current" d="M33.903 32a69.355 69.355 0 01-1.532 2.319c4.653.684 7.035 2.15 7.035 2.88 0 .971-4.22 3.248-12.406 3.248-8.185 0-12.406-2.277-12.406-3.248 0-.73 2.381-2.195 7.035-2.88A69.41 69.41 0 0120.097 32C15.506 32.852 12 34.603 12 37.199 12 41.007 19.547 43 27 43c7.454 0 15-1.993 15-5.801 0-2.596-3.504-4.347-8.097-5.199z"></path>
+                        <path class="fill-current" d="M26.994 13.304c-4.781 0-8.657 3.91-8.657 8.734 0 3.46 4.453 10.08 6.972 13.549.394.542 1.02.862 1.685.862a2.08 2.08 0 001.685-.862c2.518-3.468 6.972-10.089 6.972-13.55 0-4.823-3.876-8.733-8.657-8.733zm0 14.157c-2.825 0-5.123-2.319-5.123-5.168 0-2.85 2.298-5.17 5.123-5.17s5.123 2.32 5.123 5.17-2.298 5.168-5.123 5.168z"></path>
+                        <path class="fill-current" d="M26.994 19.149c-1.718 0-3.116 1.41-3.116 3.144 0 1.732 1.398 3.143 3.116 3.143 1.718 0 3.116-1.41 3.116-3.143 0-1.735-1.398-3.144-3.116-3.144z"></path>
+                    </g>
+                </svg>
+                <h2 class="sub-heading">Lawn mowing guide
+                </h2>
+            </div>
+        </div>
+
+        <div class="lawn-questions-second">
+            <h2 class="sub-heading">
+                When’s the best time to cut my grass?
+            </h2>
+            <p class="text">
+                The best time to mow a lawn is early in the morning or in the evening. This is because mowing during the middle of the day where temperatures are at their highest can cause the plant to go into shock. It is also recommended to mow when your grass is dry. Dry grass allows your lawn mowers to function better as well as minimize disease exposure that can spread from plant to plant. Between the hours of 8:00am - 10:00am is generally the best time to cut grass.
+            </p>
+        </div>
+
+        <div class="lawn-questions-img">
+            <img src="<?= get_img() ?>/before-after-lawn.jpg" alt="Before after lawn mowing">
+            <p class="text">
+                Mow your lawn using the one-third rule. The one-third rule says that you should never remove more than one-third of the leaf blade during any one lawn mowing. For example, if the recommended mowing height for a turfgrass is 2 inches, the height of the grass should not exceed 3 inches before it is mowed back to 2 inches.
+                <br><br>
+                Why? Turfgrasses, like other green plants, provide themselves with the necessary nutrients to grow through the process of photosynthesis. Photosynthesis is the process by which green plants and certain other organisms transform light energy (sun) into chemical energy. During photosynthesis in green plants, light energy is captured and utilized to convert water, carbon dioxide, and minerals into oxygen and energy-rich organic compounds.
+                <br><br>
+                When over one-third of the leaf blade is removed during the mowing process, there is significantly less surface area remaining to absorb the light energy to allow the process of photosynthesis to continue. Therefore, the green plant will produce less food for itself, resulting in a less healthy plant that is susceptible to insect damage and disease. Overcutting weakens the grass and also inhibits the growth of the roots, leading to the creation of a small, shallow root system. During the dry season, shallow roots can dry out which leads to the inevitable death of the plant or grass. Large root systems below ground allow grass to grow tall and healthy and can withstand the dry, summer months.
+            </p>
+        </div>
+
+        <div class="lawn-questions-second">
+            <h2>Grass cutting tips</h2>
+            <ul class="text">
+                <li>Select an appropriate mowing height. Generally speaking, the appropriate mowing height is two to three inches. Set the blade to the correct height of cut and mow frequently enough so you cut off no more than the top third of the grass plant. This will encourage stronger and healthier roots.</li>
+                <li>Don't cut too short. Cutting your lawn too short can cause your lawn to lose moisture at a more rapid pace and fosters an environment for weed and disease infestation.</li>
+                <li>Keep the lawn mower blades sharp. If you aren’t sharpening your lawn mower blades, then you could be risking the health of your lawn. Sharp blades are needed to produce a clean, even cut. Unsharpened blades have a tendency of ripping the grass tissue. Torn grass tissue can result in a tan or brown cast to the lawn. The ripping also creates a breeding ground for disease and other issues.</li>
+                <li>Leave grass clippings on your lawn. This process is called mulching, grasscycling, or recycling. Grass clippings can reduce your need for fertilizer by as much as 25% because they are full of essential nutrients. Grass clippings break down easily and cause issues only if there is an excessive amount.</li>
+                <li>Change directions. Mowing causes the grass to lie over slightly, causing mowing patterns to develop. It's important to alternate directions with each mowing to avoid the grass from lying over. Changing directions also reduces compaction and wear.</li>
+            </ul>
+        </div>
+
+        <div class="lawn-questions-second">
+            <h2>
+                What is the best lawn mower?</h2>
+            <p class="text">
+                The correct lawnmower for your grass will provide a clean cut, every time. There are several factors to consider when choosing the best mower for you.
+            </p>
+            <div class="lawn-questions-img">
+                <img src="<?= get_img() ?>/types-of-lawn-mowers.jpg" alt="bearded man mowing lawn">
+            </div>
+
+            <h2>Grass cutting tips</h2>
+            <ul class="text">
+                A push mower or walk-behind mower is ideal on 1/2 acre or less. Push Mower varieties include:
+
+                <li><b>Manual reel mower:</b> Manual push mowers are environmentally friendly and quiet. This is a good choice for small, flat lawns.</b>
+                <li><b>Electric mower:</b> Corded and cordless (battery-operated) electric mowers are less noisy than gas mowers and great for small yards. Corded mowers require access to an electrical outlet, and battery-operated types may require a re-charge mid-mow. Cordless mowers charge times will vary by battery and charger, ranging from 30 minutes to a few hours.</b>
+                <li><b>Gas-Powered push mower: </b>Ideal for medium-sized lawns, gas mowers are the conventional option for yards up to 1/2 acre in size. A gas mower is more powerful and easier to maneuver than manual and electric options and provides more power. Most crank with a pull start, but some feature an electric starter that eliminates the need to use the pull cord. Keep in mind, you will need the correct fuel.</b>
+                <li><b>Mulching mower:</b> Mulching mowers cut and drop grass clippings to the ground as you mow. They have a handful of benefits, including increasing the addition of nutrients and disbursement of beneficial microorganisms into the soil.</b>
+            </ul>
+            <p class="text">
+                A riding mower is good for a yard a 1/2 acre or larger. For large or sloped lawns, a riding mower or lawn tractor is the best option because they allow you to get a big job done quickly.
+
+            </p>
+        </div>
+
+
+    </section>
+
+
     <!-- ------------------------ -->
     <!-- Eight insured section -->
     <!-- ------------------------ -->
@@ -132,10 +299,7 @@
     <!-- ------------------------ -->
     <?php include customer_support ?>
 
-    <!-- ------------------------ -->
-    <!-- Review tenth card section -->
-    <!-- ------------------------ -->
-    <?php include reviews; ?>
+
 
     <!-- ------------------------ -->
     <!-- Footer card section -->
